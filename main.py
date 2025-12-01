7865ce82c4f7cb7e08d71ed5b1a55da818c96d48
AJFrosty <ajantefraser13@gmail.com>
2025-12-01 09:56:33 -0400

    STarted Session Management

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from DataManagement.DataManagement import DataManagement
 from Authenticator.Authenticator import Authenticator
+from SessionManagement.Sessions import SessionManager
 
 def main():
     while True:
         fileManager = DataManagement('Data/')
         auth = Authenticator(fileManager)
+        SessMgmr = SessionManager(fileManager)
         
         currentUser = auth.authenticate()
         
@@ -29,7 +31,39 @@ def main():
                 elif choice == "5":
                     fileManager.backupAll()
                 elif choice == "6":
-                    print("Session Management Dashboard(Create/Edit/Delete)")
+                    while True:
+                        print("\n====== SESSION MANAGEMENT DASHBOARD ======")
+                        print("1. Create Session")
+                        print("2. Edit Session")
+                        print("3. Delete Session")
+                        print("4. View Sessions")
+                        print("5. Add Camper to Session")
+                        print("6. Return to Admin Menu")
+                        print("==========================================")
+
+                        choice = input("Choose an option: ").strip()
+
+                        if choice == "1":
+                            SessMgmr.createSession(currentUser)
+
+                        elif choice == "2":
+                            SessMgmr.editSession(currentUser)
+
+                        elif choice == "3":
+                            SessMgmr.deleteSession(currentUser)
+
+                        elif choice == "4":
+                            SessMgmr.displaySessions()
+
+                        elif choice == "5":
+                            SessMgmr.addCamperToSessionInteractive()
+
+                        elif choice == "6":
+                            print("⬅ Returning to Admin Menu...")
+                            return
+                        else:
+                            print("Invalid option. Please try again.")
+                            
                 elif choice == "7":
                     currentUser.viewCamperInfo()
                 elif choice == "8":
